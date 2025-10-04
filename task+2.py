0b2d3db527a2d6d68e74f9e03056ca38204ba54b
shubhangiwaghchauregithub <shubhangiwaghchaure001@gmail.com>
2025-10-04 19:01:42 +0530

    assignment 1

---
+++ task+2.py
@@ -0,0 +1,23 @@
+# Problem Statement 1: Write a Python program that does the following:
+
+# 2.  Performs the basic mathematical operations on these two numbers:
+
+#Task 2: Create a Personalized Greeting
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+name= first_name + " " + last_name
+print("Hello,", name ,"! " "Welcome to Python Program")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ task_1.py
@@ -0,0 +1,27 @@
+# Problem Statement 1: Write a Python program that does the following:
+# 1.  Takes two numbers as input from the user.
+
+
+a=int(input("Enter a number:" ))
+b=int(input("Enter a number: "))
+print("Addition: ",a+b)
+print("Subtraction: ",a-b)
+print("Multiplication: ",a*b)
+print("Division: ",a/b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
